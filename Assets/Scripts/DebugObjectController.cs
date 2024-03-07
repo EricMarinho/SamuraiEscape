48b9bd223ebe2108c04b2624eefe6d72024a873d
Felipe Hernandez <132588231+fbuenohernandez@users.noreply.github.com>
2024-03-06 23:02:54 -0300

    Camera control V2
    3 tipos de controle, fixo, janela e lerp.

---
+++ Assets/Scripts/DebugObjectController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugObjectController : MonoBehaviour
+{
+
+    [SerializeField] Camera cam;
+
+    public float velocity = 0.1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            this.transform.position = new Vector3(this.transform.position.x - velocity, this.transform.position.y, 0);
+        }
+        else if (Input.GetKey("d"))
+        {
+            this.transform.position = new Vector3(this.transform.position.x + velocity, this.transform.position.y, 0);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + velocity, 0);
+        }
+        else if (Input.GetKey("s"))
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - velocity, 0);
+        }
+
+        if (Input.GetKey("r"))
+        {
+            this.transform.position = new Vector3(0, 0, 0);
+
+            cam.transform.position = new Vector3(0, this.transform.position.y + cam.orthographicSize * 0.5f, -10);
+        }
+    }
+}
