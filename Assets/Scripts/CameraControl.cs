8fcb89c3016bb9535ff4ae5e04d6031c65a1f3fd
Felipe Hernandez <132588231+fbuenohernandez@users.noreply.github.com>
2024-04-01 09:26:18 -0300

    Ice tileset with slide

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -33,8 +33,8 @@ public class CameraControl : MonoBehaviour
         cameraWidth = Camera.main.aspect * cameraHeight;
         camPos = Camera.main.transform.position;
         destination = camPos;
-        //horizontalMovAmount = cameraWidth;
-        //verticalMovAmount = cameraHeight;
+        horizontalMovAmount = cameraWidth;
+        verticalMovAmount = cameraHeight;
     }
 
     void moveCamera()
