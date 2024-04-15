3e64b50cf1b1aadbf4f3f07d0c08ebf39d516b42
Felipe Hernandez <132588231+fbuenohernandez@users.noreply.github.com>
2024-04-15 08:06:24 -0300

    Resolution, game start, quit
    Volume control not working for some reason

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
@@ -11,33 +10,42 @@ public class MenuController : MonoBehaviour
     public Canvas credits;
     public Dropdown resolutionDropdown;
     public Slider slider;
+    public Toggle fullScreenToggler;
+    public Toggle screenSizeToggler;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        setCanvasHome();
-    }
+    int h, w;
+    bool windowed = false;
 
-    enum enumResolution
-    {
-        option1,
-        option2,
-        option
-    }
+    public AudioSource audio;
+    public AudioSource bgm;
+
+    [SerializeField] AudioMixer masterMixer;
 
     static int[] option1 = new int[2] { 640, 480 };
     static int[] option2 = new int[2] { 800, 800 };
     static int[] option3 = new int[2] { 1024, 768 };
 
-    int[][] resolutions = new int[][] {option1, option2, option3} ;
+    int[][] resolutions = new int[][] { option1, option2, option3 };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        w = resolutions[2][0];
+        h = resolutions[2][1];
+
+        bgm.Play();
+        setCanvasHome();
+    }
 
     public void startGame()
     {
         Debug.Log("START");
+        SceneManager.LoadScene("SampleScene");
     }
-
+    
     public void setCanvasHome()
     {
+        audio.Play();
         home.gameObject.SetActive(true);
         options.gameObject.SetActive(false);
         credits.gameObject.SetActive(false);
@@ -46,6 +54,7 @@ public class MenuController : MonoBehaviour
 
     public void setCanvasOptions()
     {
+        audio.Play();
         home.gameObject.SetActive(false);
         options.gameObject.SetActive(true);
         credits.gameObject.SetActive(false);
@@ -54,6 +63,7 @@ public class MenuController : MonoBehaviour
 
     public void setCanvasCredits()
     {
+        audio.Play();
         home.gameObject.SetActive(false);
         options.gameObject.SetActive(false);
         credits.gameObject.SetActive(true);
@@ -63,16 +73,36 @@ public class MenuController : MonoBehaviour
     public void exitGame()
     {
         Debug.Log("EXIT");
+        Application.Quit();
     }
 
     public void resolutionChange()
     {
-        Debug.Log(resolutionDropdown.value);
+        audio.Play();
+
+        w = resolutions[resolutionDropdown.value][0];
+        h = resolutions[resolutionDropdown.value][1];
+
+        Screen.SetResolution(w, h, windowed);
+        Debug.Log(resolutionDropdown.value + " " + w + " " + h);
     }
 
     public void sliderChange()
     {
         Debug.Log(slider.value);
+
+        float volume = 20f;
+
+        volume = volume > 0 ? volume * slider.value - 20f : 0;
+
+        masterMixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void fullscreenToggle()
+    {
+        audio.Play();
+        Debug.Log(fullScreenToggler.isOn);
+        Screen.SetResolution(w,h, windowed);
     }
 
     // Update is called once per frame
@@ -80,4 +110,5 @@ public class MenuController : MonoBehaviour
     {
 
     }
-}
+
+ }
